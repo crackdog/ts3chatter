635ad4b7f2fe30c1ce3303996d4b11c0ac23081e
Florian Kahllund <flo.kahllund@gmail.com>
2014-06-27 23:05:28 +0200

    adding login flags

--- main.go
+++ main.go
@@ -17,12 +17,16 @@ func main() {
 		fcgiport   string
 		address    string
 		logger     *log.Logger
+		lname      string
+		lpw        string
 	)
 
 	flag.BoolVar(&loggerFlag, "log", false, "enable stdout logger")
 	flag.StringVar(&fcgiport, "fcgiport", "9001", "change fcgi port")
 	flag.StringVar(&address, "ts3addr", "localhost",
 		"change the ts3 server query 'address:port'")
+	flag.StringVar(&lname, "login", "ts3chatter", "set ts3 server query login name")
+	flag.StringVar(&lpw, "pw", "********", "set ts3 server query password")
 
 	flag.Parse()
 
@@ -30,7 +34,11 @@ func main() {
 		logger = ts3sqlib.StdoutLogger
 	}
 
-	ts3, err := server.New(address, "testlogin", "bwu7tzVh", 1, logger, 5)
+	//for my test configuration:
+	//lname = "testlogin"
+	//lpw = "bwu7tzVh"
+
+	ts3, err := server.New(address, lname, lpw, 1, logger, 5)
 	if err != nil {
 		//fmt.Fprintln(os.Stderr, err)
 		log.Fatal(err)
