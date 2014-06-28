04e92cff5b41096051035fd4d113ed99adc420c8
Florian Kahllund <flo.kahllund@gmail.com>
2014-06-29 00:01:45 +0200

    fixing bug

--- server/handler.go
+++ server/handler.go
@@ -10,27 +10,28 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//s.handlermutex.Lock()
 	//defer s.handlermutex.Unlock()
 
+	var i int
+
 	if !s.closed {
 		s.clmutex.Lock()
 		clients := s.clientlist
 		s.clmutex.Unlock()
 
-		if len(clients) > 0 {
-			fmt.Fprintln(w, "<p>No one is online right now.</p>")
-		} else {
-			for i := range clients {
-				nick, ok := clients[i]["client_nickname"]
-				if ok {
-					if strings.Contains(clients[i]["client_type"], "0") {
-						fmt.Fprintln(w, "<p>", nick, "</p>")
-					}
-				} else {
-					fmt.Fprintln(w, "error: ", clients[i], ", empty map")
-					if s.logger != nil {
-						s.logger.Print("error: " + fmt.Sprint(clients[i]))
-					}
+		for i = range clients {
+			nick, ok := clients[i]["client_nickname"]
+			if ok {
+				if strings.Contains(clients[i]["client_type"], "0") {
+					fmt.Fprintln(w, "<p>", nick, "</p>")
+				}
+			} else {
+				fmt.Fprintln(w, "error: ", clients[i], ", empty map")
+				if s.logger != nil {
+					s.logger.Print("error: " + fmt.Sprint(clients[i]))
 				}
 			}
+		}
+		if i == 0 {
+			fmt.Fprintln(w, "<p>No one is online right now.</p>")
 		}
 	} else {
 		http.Error(w, "internal error", 500)
